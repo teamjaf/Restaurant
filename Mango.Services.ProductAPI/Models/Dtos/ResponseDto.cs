fec1d488a6b697aa81b88675c9e5fa737a636590
teamjaf <65031312+teamjaf@users.noreply.github.com>
2022-04-20 11:03:46 +0100

    adding interface

---
+++ Mango.Services.ProductAPI/Models/Dtos/ResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.ProductAPI.Models.Dtos
+{
+    public class ResponseDto
+    {
+        public bool IsSuccess { get; set; } = true;
+        public string? Result { get; set; }
+        public string DisplayMessage { get; set; } = " ";
+        public List<string>? ErrorMessages { get; set; }
+
+    }
+}
--- Mango.Services.ProductAPI/Repository/iProductRepository.cs
+++ Mango.Services.ProductAPI/Repository/iProductRepository.cs
@@ -1,1 +1,6 @@
-﻿
+﻿namespace Mango.Services.ProductAPI.Repository;
+
+public interface iProductRepository
+{
+     
+}
